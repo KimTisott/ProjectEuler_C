bb6f7cbdb248da2a49a8a3e95118184472eca761
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-06-06 11:21:55 -0300

    Update problem3.c

--- Solutions/problem3.c
+++ Solutions/problem3.c
@@ -1,8 +1,8 @@
 void problem3()
 {
-	unsigned long long result = 600851475143;
+	long long result = 600851475143;
 
-	for (unsigned long long i = 2; i < result; i++)
+	for (long long i = 2; i < result; i++)
 	{
 		while (result % i == 0)
 		{
