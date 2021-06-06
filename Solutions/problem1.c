f7f127903a9de9e02215f133935cae744f8be46a
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-06-06 10:33:58 -0300

    Update problem1.c

--- Solutions/problem1.c
+++ Solutions/problem1.c
@@ -1,20 +1,20 @@
 void problem1()
 {
-	int sum = 0;
+	int result = 0;
 
 	int current = 3;
 
 	for (int i = 3; i < 1000; i += 3)
 	{
-		sum += i;
+		result += i;
 	}
 
 	for (int i = 5; i < 995; i += 15)
 	{
-		sum += i;
+		result += i;
 
-		sum += i + 5;
+		result += i + 5;
 	}
 
-	sum += 995;
+	result += 995;
 }
