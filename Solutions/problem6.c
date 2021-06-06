46e9cee1a2460e9c5d63704ede9950b23347d210
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-06-06 15:39:03 -0300

    Create problem6.c

---
+++ Solutions/problem6.c
@@ -0,0 +1,17 @@
+void problem6()
+{
+    int result = 0;
+
+    int sum = 0;
+
+    int sumSquare = 0;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        sum += i;
+
+        sumSquare += i * i;
+    }
+
+    result = sum * sum - sumSquare;
+}
