941c3de4e415ed2c905b04762de5cb122ea6b57f
KimTisott <40810123+KimTisott@users.noreply.github.com>
2021-06-10 00:38:04 -0300

    Update benchmark.c

--- Benchmark/benchmark.c
+++ Benchmark/benchmark.c
@@ -31,16 +31,11 @@ void benchmark(void* function(), int count, int progress)
 		times[i] = current;
 
 		total += current;
-	}
 
-	if (progress)
-	{
-		for (int i = 0; i < count; i++)
+		if (progress)
 		{
 			printf("[%04d] %.1fus\n", i + 1, times[i] * 1000000);
 		}
-
-		printf("------------------------------\n");
 	}
 
 	mean = total / count;
@@ -51,6 +46,8 @@ void benchmark(void* function(), int count, int progress)
 	}
 
 	printf("Total: %.3fus\n", total * 1000000);
+
 	printf("Mean: %.3fus\n", mean * 1000000);
+
 	printf("SD: %.3fus\n", sqrt(sd / count) * 1000000);
 }
